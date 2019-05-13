a73c3d5e8200a35177ecf2b90fca3534c1d9801a
SoBoeckler <33057744+SoBoeckler@users.noreply.github.com>
2019-05-13 15:29:25 +0200

    Add files via upload

--- second_steps.py
+++ second_steps.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 15:27:14 2019
+
+@author: Admin
+"""
+
 # Exercise 2
 
 # Satellites:
@@ -10,12 +17,26 @@ sat_database = {"METEOSAT" : 3000,
 
 # 1) Add the "GOES" and "worldview" satellites with their 2000/0.31m resolution to the dictionary [2P]
 
+sat_database.update({"GOES":2000,"worldview":0.31})
+
 print("I have the following satellites in my database:")
 
 # 2) print out all satellite names contained in the dictionary [2P]
+    
+for x in sat_database:
+    print(x)
 
 # 3) Ask the user to enter the satellite name from which she/he would like to know the resolution [2P]
 
-# 4) Check, if the satellite is in the database and inform the user, if it is not [2P]
+eingabe=input("From which satellite you like to know the resolution: ")
 
-# 5) If the satellite name is in the database, print a meaningful message containing the satellite name and it's resolution [2P] 
+print(sat_database.get(eingabe))
+
+# 4) Check, if the satellite is in the database and inform the user, if it is not [2P]
+if eingabe in sat_database.keys():
+# 5) If the satellite name is in the database, print a meaningful message containing the satellite name and it's resolution [2P]
+    #print("The resolution for", eingabe,"is",sat_database.get(eingabe))
+    print("The resolution for {0} is {1:5.2f}.".format (eingabe,sat_database.get(eingabe)))
+else:
+    print("Your satellite is not in the dictionary - please try again!")
+
